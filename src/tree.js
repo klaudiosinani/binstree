0ba2ebf27e255b7f89cc61f7a1a3a1980e63f3cb
Klaus Sinani <klaussinani@gmail.com>
2019-04-07 23:55:48 +0300

    Add new `Tree#insert(...values)` class method

--- src/tree.js
+++ src/tree.js
@@ -1,4 +1,5 @@
 'use strict';
+const Node = require('./node');
 
 class Tree {
   constructor() {
@@ -7,6 +8,36 @@ class Tree {
 
   get root() {
     return this._root;
+  }
+
+  _insert(node, target) {
+    if (node.value < target.value) {
+      if (target.left) {
+        return this._insert(node, target.left);
+      }
+
+      target.left = node;
+    } else {
+      if (target.right) {
+        return this._insert(node, target.right);
+      }
+
+      target.right = node;
+    }
+  }
+
+  insert(...values) {
+    values.forEach(value => {
+      const {_root} = this;
+      const node = new Node(value);
+
+      if (_root) {
+        return this._insert(node, _root);
+      }
+
+      this._root = node;
+    });
+    return this;
   }
 }
 
