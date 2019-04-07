4f1bc5a9174f48d81584aeaf1b5382b523f83e79
Klaus Sinani <klaussinani@gmail.com>
2019-04-07 20:57:16 +0300

    TypeScript: Initialize ambient declarations

---
+++ types/bstrie.d.ts
@@ -0,0 +1,31 @@
+declare namespace node {
+  export interface Constructor {
+    new <T = any>(value?: T): Instance<T>;
+  }
+
+  export interface Instance<T> {
+    value: T;
+    left: Instance<T> | null;
+    right: Instance<T> | null;
+  }
+}
+
+declare namespace tree {
+  export interface Constructor {
+    new <T = any>(): Instance<T>;
+  }
+
+  export interface Instance<T> {
+    readonly root: node.Instance<T> | null;
+  }
+}
+
+declare namespace bstrie {
+  export interface Node<T = any> extends node.Instance<T> {}
+  export interface Tree<T = any> extends tree.Instance<T> {}
+}
+
+declare const bstrie: {
+  Node: node.Constructor;
+  Tree: tree.Constructor;
+};
