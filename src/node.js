b517d062edfadcde46ba9d9abb961bfe74e5cc41
Klaus Sinani <klaussinani@gmail.com>
2019-04-20 22:50:31 +0300

    Minor improvements on `Tree#right` class property mutator (#3)

--- src/node.js
+++ src/node.js
@@ -47,6 +47,10 @@ class Node {
   }
 
   set right(node) {
+    if (node.key <= this.key) {
+      throw new Error('Right child node key must be greater than the parent node key');
+    }
+
     this._right = node;
   }
 
